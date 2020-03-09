359799e9fc3562f769321cabe5aa31d20f5bca44
farukweb <faruk.webdev01@gmail.com>
2020-03-09 22:28:44 +0600

    null-vs-undefine

---
+++ null-vs-undefine.js
@@ -0,0 +1,30 @@
+// let paki;
+// console.log(paki);
+
+// =================================
+
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
+// const result= add(13);
+// console.log(result);
+
+// ========================================
+
+/** 
+const premik ={name: "smart shankar", phone:224434};
+console.log(premik.gf);
+
+*/
+// =======================================
+
+/** 
+let fun =undefined;
+console.log(fun);
+
+let ages=null;
+console.log(ages)
+*/
+
+let man = [2, 4, 5]
+console.log(man[22]);
