9ce357c6cbfd0b8d0ea16b0883d3c9949d940ad7
farukweb <faruk.webdev01@gmail.com>
2020-03-09 22:00:42 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,29 @@
+// const age = 4;
+// if(age){
+//     console.log('Condition is ture');
+// }else{
+//     console.log('Condition is false');
+// }
+
+
+// =================string element example===========
+
+
+// const name = 'Faruk Khan';
+// const name1 ='';
+// const name1 ='0'
+// const nameone =[]
+// if (nameone){
+//     console.log('Condition is true');
+// }else{
+//     console.log('Condition is false');
+// }
+
+// ======================Truw-False=================
+let name =0;
+if(name || name == 0){
+    console.log('Condition is true');
+}else{
+    console.log('Condition is false')
+}
+
